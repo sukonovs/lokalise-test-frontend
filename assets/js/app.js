a03959e299a86f8a017bac1b9357fc5f0cacbceb
Roberts Sukonovs <roberts.sukonovs@gmail.com>
2020-01-11 19:11:20 +0200

    Complete guest book.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,82 @@
 import '../css/app.scss';
 import $ from 'jquery';
-import 'bootstrap';
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 
-$(document).ready(function () {
-    $("h1").text("jquery value");
+Vue.use(BootstrapVue);
+
+new Vue({
+    el: "#app",
+    delimiters: ['{', '}'],
+    data: {
+        form: {
+            email: '',
+            text: '',
+        },
+        comments: [],
+        title: 'Guest Book',
+        error: false,
+        errorMessage: '',
+        success: false,
+        successMessage: '',
+    },
+    created: function () {
+        $.ajax({
+            url: '/comment',
+            context: this,
+            dataType: 'json',
+            success: function (data) {
+                this.comments = data;
+            },
+            error: function () {
+                this.showErrorMessage('Could not fetch messages.');
+            },
+        });
+    },
+    methods: {
+        onSubmit(evt) {
+            evt.preventDefault();
+
+            $.ajax({
+                url: '/comment/new',
+                type: 'POST',
+                context: this,
+                dataType: 'json',
+                data: this.form,
+                success: function () {
+                    this.comments.push(
+                        {
+                            email: this.form.email,
+                            text: this.form.text,
+                        },
+                    );
+
+                    this.form.text = '';
+                    this.form.email = '';
+                    document.getElementById("comment-form").reset();
+
+                    this.showSuccessMessage('Comment has been added.');
+                },
+                error: function (data) {
+                    let errorMessage = 'Invalid field values:';
+                    for (const [field, errors] of Object.entries(data.responseJSON)) {
+                        errors.forEach((error) => {
+                            errorMessage = errorMessage.concat(`<br>${field}: ${error}.`);
+                        });
+                    }
+                    this.showErrorMessage(errorMessage)
+                },
+            });
+        },
+        showSuccessMessage: function (message) {
+            this.error = false;
+            this.success = true;
+            this.successMessage = message;
+        },
+        showErrorMessage: function (message) {
+            this.error = true;
+            this.success = false;
+            this.errorMessage = message;
+        }
+    }
 });
